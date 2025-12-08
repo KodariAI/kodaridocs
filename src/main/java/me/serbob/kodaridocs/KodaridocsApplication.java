a769040dee4923606c1b1749b9631f1f309c3465
Steal a Baddie <serbanvlad19@gmail.com>
2025-12-08 19:55:23 +0200

    Bump

--- src/main/java/me/serbob/kodaridocs/KodaridocsApplication.java
+++ src/main/java/me/serbob/kodaridocs/KodaridocsApplication.java
@@ -1,13 +1,15 @@
 package me.serbob.kodaridocs;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodaridocsApplication {
 
-    // main
     public static void main(String[] args) {
+        Dotenv.configure().ignoreIfMissing().systemProperties().load();
+
         SpringApplication.run(KodaridocsApplication.class, args);
     }
 }
