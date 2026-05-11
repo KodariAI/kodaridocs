44354080cabe8999397bf9e8d2e7346eb10af33a
SerbanHiro <serbanvlad19@gmail.com>
2026-05-11 22:28:33 +0300

    test1

--- app/src/main/java/me/serbob/kodaridocs/KodaridocsApplication.java
+++ app/src/main/java/me/serbob/kodaridocs/KodaridocsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodaridocsApplication {
 
-    public static void main(
+    static void main(
             String[] args
     ) {
         Dotenv.configure().ignoreIfMissing().systemProperties().load();
